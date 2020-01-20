cd3de87e7938ff9c67076891fdaa31855276739e
Erth <panot1997@hotmail.com>
2020-01-21 01:39:43 +0700

    New year, New UI.

--- js/erth.js
+++ js/erth.js
@@ -55,6 +55,12 @@ ScrollReveal().reveal('.card6', { delay: 1200 });
 ScrollReveal().reveal('.card7', { delay: 1400 });
 ScrollReveal().reveal('.card8', { delay: 1600 });
 
+
+ScrollReveal().reveal('.delay', { delay: 3000 });
+ScrollReveal().reveal('.resume-image', { delay: 100 });
+ScrollReveal().reveal('.exp1', { delay: 200 });
+ScrollReveal().reveal('.exp2', { delay: 400 });
+ScrollReveal().reveal('.exp3', { delay: 600 });
 /* Work details */
 $(window).scroll(function() {
 	var wScroll = $(this).scrollTop();
@@ -65,8 +71,45 @@ $(window).scroll(function() {
 	  transform: "translate(0px, " + wScroll / 4 + "%)"
 	});
 	 $('.fore-bird').css({
-	 'transform':'translate(0px, -'+ wScroll / 40 +'%)'
+	 transform: "translate(0px, -'+ wScroll / 40 +'%)"
    });
   });
   
-/* full page scroll */
+/* Text Wrapper */
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  setTimeout(
+	function() 
+	{
+	    $('.ml11').remove();
+	}, 3000);
